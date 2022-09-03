2b74a793cf4fa6132a2323a0f35ca33511168e4d
ad3rinto <aad3rinto@gmail.com>
2022-09-03 13:54:38 +0100

    Add function to view all inserted documents

--- app.js
+++ app.js
@@ -18,9 +18,13 @@ client.connect(function(err){
 
     const db = client.db(dbName);
 
-    insertDocuments(db, function(){
+    findDocuments(db, function(){
         client.close();
-    });
+    })
+
+    // insertDocuments(db, function(){
+    //     client.close();
+    // });
 
 });
 
@@ -51,5 +55,18 @@ const insertDocuments = function(db, callback) {
         assert.equal(3, result.ops.length);
         console.log("Inserted 3 documents into the collection");
         callback(result);
+    });
+}
+
+
+const findDocuments = function(db, callback){
+    //get the documents collection
+    const collection = db.collection("fruits");
+    //find some documents
+    collection.find({}).toArray(function(err, fruits){
+        assert.equal(err, null);
+        console.log("Found the following records");
+        console.log(fruits);
+        callback(fruits);
     });
 }
